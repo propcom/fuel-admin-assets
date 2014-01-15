bb171c5a94b78d2c479e923f509ec47739830b3e
Mitchell Ward <mitchell.ward@propcom.co.uk>
2014-01-15 11:34:50 +0000

    Make icons in buttons larger

--- js/wordlist.js
+++ js/wordlist.js
@@ -1,52 +1,70 @@
 $.fn.wordlist = function(){
-	var $field = this;
 
-	var word;
-	var words = []; // words is an array
-	if ($field.val().length > 0) {
-		words = $field.val().split(','); // Fill array if textarea has contents
-		cleanwords = [];
-		$.each(words, function(){
-			cleanwords.push(this.trim());
-		});
-		words = cleanwords;
-		$field.val(words.join(','));
-	}
+	return this.each(function(){
+		// Elements to work with:
+		var $field = $(this), // textarea
+			$mask = $('<div class="wordlist-view" />'), // Styled view
+			$input = $('<input type="text" class="js-wordlist-input" />'); // input
 
-	$('<div class="wordlist-view" />').insertAfter($field); // Add the display div
-	$field.hide(); // Hide the textarea
+		// Vars to use:
+		var word,
+			words = [];
 
-	$.each(words, function(i, word){
-		$('<span>'+word.trim()+'</span>').appendTo('.wordlist-view');
-	});
-	$('<input type="text" class="js-wordlist-input" />').appendTo('.wordlist-view');
-
-	$('.wordlist-view').on('click', 'span', function(){
-		var $word = $(this); // Word span object
-		word = $word.text().trim(); // Word text
-		var i = words.indexOf(word); // Position of word in array
-
-		words.splice(i, 1); // Remove word from array
-
-		$word.remove(); // Remove word object from div
-
-		$field.val(words.join(',')); // Update text in the textarea
-	});
-
-	$('.js-wordlist-input').on('keyup', function(e) {
-		var word = $(this).val().trim();
-		if (e.which == 13 && word != '') {
-			e.preventDefault();
-			e.stopPropagation();
-			// Add word
-			words.push(word);
-
-			$('<span>'+word.trim()+'</span>').insertBefore('.js-wordlist-input');
-
-			$(this).val('');
-			console.log(words);
-
-			$field.val(words.join(',')); // Update text in the textarea
+		if ($field.val().length > 0) {
+			words = $field.val().split(',');
+			cleanwords = [];
+			$.each(words, function(){
+				cleanwords.push(this.trim());
+			});
+			words = cleanwords;
+			$field.val(words.join(','));
 		}
+
+		// Put the styled view in and hide the textarea
+		$mask.insertAfter($field);
+		$field.hide();
+
+		$.each(words, function(i, word){
+			// Add the words to the styled view
+			$('<span>'+word.trim()+'</span>').appendTo($mask);
+		});
+
+		// Put the input in after all of the words
+		$input.appendTo($mask);
+
+		// Delete a word when you click it
+		$mask.on('click', 'span', function(){
+			var $word = $(this);
+
+			// Find position of word in the array of words
+			word = $word.text().trim();
+			var i = words.indexOf(word);
+
+			// Remove word from array
+			words.splice(i, 1);
+
+			// Remove the word from the styled view
+			$word.remove();
+
+			// Update the contents of the textarea
+			$field.val(words.join(','));
+		});
+
+		$input.on('keyup', function(e) {
+			// Build up what the user is typing
+			var word = $(this).val().trim();
+
+			if (e.which == 13 && word != '') {
+				// When they press enter, add the word to the array
+				words.push(word);
+				// Add to the display view
+				$('<span>'+word.trim()+'</span>').insertBefore($input);
+				// Update the contents of the textarea
+				$field.val(words.join(','));
+
+				// Clear the input
+				$(this).val('');
+			}
+		});
 	});
 };
