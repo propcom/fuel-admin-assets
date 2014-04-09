37de08aca8224258fa51e46a025897a25491bc4d
kwok <kwokhingli@hotmail.com>
2014-04-09 12:19:06 +0100

    Modal alert fix

--- js/admin.js
+++ js/admin.js
@@ -331,7 +331,6 @@ $(function(){
 		if (content.form && content.form.ajax !== false) {
 			$modal.find('.js-modal-submit').on('click', function (e) {
 				e.preventDefault();
-
 				$.ajax({
 					url: content.form.action || '',
 					type: content.form.method || 'post',
@@ -347,6 +346,7 @@ $(function(){
 						$(document).trigger('complete.submit.modal.admin', arguments);
 						$(document).trigger('always.submit.modal.admin', arguments);
 					});
+				$('.modal-body .alert').show();
 			});
 		}
 
