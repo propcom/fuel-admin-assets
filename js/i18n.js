6d2d08b9c97bf6393d998ad9f619268d7c92a8bc
Simon Moore <simon.moore@propcom.co.uk>
2014-07-31 16:26:31 +0100

    Update Styling of SVG translation pue charts

--- js/i18n.js
+++ js/i18n.js
@@ -19,11 +19,19 @@ $(function(){
 				strokewidth: 0,
 				init: true
 			});
+		} else if (translated == 0) {
+			var outstanding = 100;
+
+			chart.piechart(105, 75, 75, [i], {
+				colors: ['#272727', '#272727'],
+				strokewidth: 0,
+				init: true
+			});
 		} else {
 			var outstanding = (awaiting / total)*100;
 
 			chart.piechart(105, 75, 75, [translated, awaiting], {
-				colors: ['#000', '#BBB'],
+				colors: ['#4cc0bf', '#4cc0bf'],
 				strokewidth: 0,
 				init: true
 			});
